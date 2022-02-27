4d2fe1b74443a999a8e3c5e1414e07e2f75579af
Sandeep <69573725+SandeepMullangi@users.noreply.github.com>
2022-02-27 16:52:33 +0530

    Create Insert Record using Anonymous Block.sql

---
+++ PL SQL Basics/Insert Record using Anonymous Block.sql
@@ -0,0 +1,9 @@
+DECLARE
+      l_department_id  department.department_id%TYPE;
+    BEGIN
+       SELECT MAX (department_id) + 10 INTO l_department_id FROM department;
+  
+       INSERT INTO department (department_id, department_name, location_id)
+            VALUES (l_department_id, 'TESTING', 'CHN-102');
+    END;
+    /
